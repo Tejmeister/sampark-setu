e36da42f2978b5abd2c9fbfb414db6d51b82faef
Tejas Parmar <Tejas.Parmar@contractor.p66.com>
2021-09-27 12:52:06 +0530

    Added Initial Files

---
+++ src/main/java/com/tejas/samparksetu/SamparkSetuApplication.java
@@ -0,0 +1,13 @@
+package com.tejas.samparksetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamparkSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamparkSetuApplication.class, args);
+	}
+
+}
